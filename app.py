477e5cc22315b9001e05b665d1151d27196ce6bd
Anders Damsgaard <andersdm@gmail.com>
2015-08-13 15:32:24 +0200

    requirements test removed

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 @app.route('/')
 def hello():
     print(os.environ['APP_SETTINGS'])
-    return "Hello World!"
-
 
 @app.route('/<name>')
 def hello_name(name):
