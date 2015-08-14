d632757f36d99543bd9042263a296bf21797244c
Anders Damsgaard <andersdm@gmail.com>
2015-08-14 14:46:22 +0200

    added database

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
+from sqlalchemy.dialects.postgresql import JSON
+from rq import Queue
+from rq.job import Job
+from worker import conn
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-
+q = Queue(connection=conn)
+db = SQLAlchemy(app)
+from models import Result
 
 @app.route('/')
 def hello():
-    print(os.environ['APP_SETTINGS'])
+    return "Hello World!"
+
 
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
 
 if __name__ == '__main__':
     app.run()
