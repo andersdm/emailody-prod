7088f00996148bf256385e3e6c5ba98d644bea7e
Anders Damsgaard <andersdm@gmail.com>
2015-08-27 11:14:59 +0200

    everything changed - removed test

--- app.py
+++ app.py
@@ -282,68 +282,6 @@ def get_messages(contactId,pagenr):
     return resp
 
 
-@app.route("/v1/gmail/messages2/<int:contactId>/<int:pagenr>", methods=['GET'])
-def get_messages2(contactId,pagenr):
-    for contact in session['seen']:
-        credentials = client.OAuth2Credentials.from_json(session['credentials'])
-        http = credentials.authorize(httplib2.Http(cache=".cache"))
-
-# Build the Gmail service from discovery
-
-        gmail_service = discovery.build('gmail', 'v1', http=http)
-    
-        def mailscallbackfunc(result, results, moreresults):
-
-       # in old Python versions:
-       # if seen.has_key(marker)
-       # but in new ones:
-            if 'UNREAD' in results['labelIds']:
-                Unread = 'true'
-            else:
-                Unread = 'false'
-            for header in results['payload']['headers']:
-                if header['name'] == 'Date':
-                    Date = header['value']
-                if header['name'] == 'From':
-                    From = header['value']
-                    if From == "Anders Damsgaard <andersdm@gmail.com>": #SKAL ÆNDRES
-                        Sent = True
-                    else:
-					    Sent = False
-                if header['name'] == 'Subject':
-                    Subject = header['value']
-                
-            Contact = {
-                'messageId': results['id'],
-                'date': Date,
-                'subject': Subject,
-                'snippet': results['snippet'],
-                'unread': Unread,
-                'sent': Sent
-                }
-        
-            session['mails'].append(Contact)
-            
-    #for msg_id in message_ids['messages']:
-    #    batchContacts.add(gmail_service.users().messages().get(userId='me',
-    #              id=msg_id['id'], format='metadata',
-    #              metadataHeaders=['from', 'date']))
-    #batchContacts.execute()
-    
-        query = "\"to:'" + contact + "' AND from:me \" OR from:'" + contact + "'"
-        message_ids = gmail_service.users().messages().list(userId='me',
-                maxResults=10, labelIds='INBOX', q=query).execute()
-    
-        batchMails = BatchHttpRequest(callback=mailscallbackfunc)
-        for msg_id in message_ids['messages']:
-            batchMails.add(gmail_service.users().messages().get(userId='me',
-                      id=msg_id['id'], format='metadata',
-                      metadataHeaders=['from', 'date', 'subject']))
-        batchMails.execute()
-    
-    js = json.dumps(session['mails'])
-    resp = Response(js, status=200, mimetype='application/json')
-    return resp
     
 #if __name__ == '__main__':
 #    app.config['SECRET_KEY'] = 'super secret key'
