bfdc1e3ac092de53bc8f94f6bcf64bba7edb2a78
Anders Damsgaard <andersdm@gmail.com>
2015-08-27 11:33:47 +0200

    added animation

--- static/app.js
+++ static/app.js
@@ -8,7 +8,8 @@ angular.module('myApp', [
   'myApp.view2',
   'myApp.version',
   'lumx',
-  'ui.gravatar'
+  'ui.gravatar',
+  'ngAnimate'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
