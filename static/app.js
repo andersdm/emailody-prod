1cbb3550bb1797c71a0f86a84b3c18c00ef19880
Anders Damsgaard <andersdm@gmail.com>
2015-08-31 15:27:16 +0200

    added avatars

--- static/app.js
+++ static/app.js
@@ -20,6 +20,6 @@ angular.module('ui.gravatar').config([
   'gravatarServiceProvider', function(gravatarServiceProvider) {
     gravatarServiceProvider.defaults = {
       size     : 100,
-      "default": 'http://emailody-stage.herokuapp.com/static/img/avatars/avatar2.png'  // Mystery man as default for missing avatars
+      "default": 'http://emailody-stage.herokuapp.com/static/img/avatars/avatar3.png'  // Mystery man as default for missing avatars
     };
 }]);
