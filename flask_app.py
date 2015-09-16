c0a73676a37f5f8f16c8017b1e03f06226eb0052
Anders Damsgaard <andersdm@gmail.com>
2015-09-16 08:24:32 +0000

    Working encodings

--- flask_app.py
+++ flask_app.py
@@ -1,48 +1,30 @@
-# encoding=utf8
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
-import os
-import logging
-import sys
 import httplib2
 import re
 import json
 import redis
 import base64
-import chardet
+import quopri
 from premailer import transform
 from apiclient import errors
 from flask import Flask, redirect, url_for, Response, render_template, session, request
-#from flask.ext.sqlalchemy import SQLAlchemy
-#from sqlalchemy.dialects.postgresql import JSON
-#from rq import Queue
-#from rq.job import Job
-#from worker import conn
-redis_url = 'redis://rediscloud:u1ODHW41noA8sqMG@pub-redis-16921.us-east-1-4.6.ec2.redislabs.com:16921'
-conn = redis.StrictRedis.from_url(redis_url)
 from googleapiclient import discovery
 from googleapiclient.http import BatchHttpRequest
 from oauth2client import client
 from flask_kvsession import KVSessionExtension
 from simplekv.memory.redisstore import RedisStore
+from fixbadunicode import fix_bad_unicode
+redis_url = 'redis://rediscloud:u1ODHW41noA8sqMG@pub-redis-16921.us-east-1-4.6.ec2.redislabs.com:16921'
+conn = redis.StrictRedis.from_url(redis_url)
 store = RedisStore(conn)
 app = Flask(__name__)
 KVSessionExtension(store, app)
-#app.config['SESSION_TYPE'] = 'memcached'
 app.config['SECRET_KEY'] = 'super secret key'
-#app.debug = True
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
-#app.config.from_object(os.environ['APP_SETTINGS'])
-#db = SQLAlchemy(app)
-
-#q = Queue(connection=conn)
-#from contacts import get_messages
-
-#from models import *
-
-
 
 @app.route('/')
 def index():
@@ -51,9 +33,7 @@ def index():
     session['contacts'] = []
     session['seen'] = []
     session['mails'] = []
-    session['messageIds'] = []
     session['maillist'] = []
-    session['messagelist'] = []
 
     if 'credentials' not in session:
         return redirect(url_for('oauth2callback'))
@@ -64,12 +44,11 @@ def index():
         return render_template('index.html')
 
 
-
 @app.route('/oauth2callback')
 def oauth2callback():
-    flow = client.flow_from_clientsecrets('/home/Andersdm/emailody/client_secrets.json',
-            scope='https://www.googleapis.com/auth/gmail.readonly',
-redirect_uri=url_for('oauth2callback',_external=True))
+    flow = client.flow_from_clientsecrets('/home/Andersdm/emailody/client_secrets.json'
+            , scope='https://www.googleapis.com/auth/gmail.readonly',
+            redirect_uri=url_for('oauth2callback', _external=True))
     if 'code' not in request.args:
         auth_uri = flow.step1_get_authorize_url()
         return redirect(auth_uri)
@@ -82,19 +61,18 @@ redirect_uri=url_for('oauth2callback',_external=True))
 
 @app.route('/v1/gmail/contacts/<int:pagenr>')
 def contacts(pagenr):
-    # Authorize the httplib2.Http object with our credentials
-    credentials = client.OAuth2Credentials.from_json(session['credentials'])
 
-    http = credentials.authorize(httplib2.Http(cache=".cache"))
+    # Authorize the httplib2.Http object with our credentials
+
+    credentials = client.OAuth2Credentials.from_json(session['credentials'])
+    http = credentials.authorize(httplib2.Http(cache='.cache'))
 
 # Build the Gmail service from discovery
 
     gmail_service = discovery.build('gmail', 'v1', http=http)
-    while (len(session['seen'])+1 < pagenr*10):
+    while len(session['seen']) + 1 < pagenr * 10:
 
 # Retrieve a page of threads
-
-
 
 # Print ID for each thread
 # if threads['threads']:
@@ -102,10 +80,6 @@ def contacts(pagenr):
 #    print 'Thread ID: %s' % (thread['id'])
 
         def contactscallbackfunc(result, results, moreresults):
-
-       # in old Python versions:
-       # if seen.has_key(marker)
-       # but in new ones:
             if 'UNREAD' in results['labelIds']:
                 Unread = True
             else:
@@ -119,11 +93,8 @@ def contacts(pagenr):
                     address = re.sub(r'[<>]', '', address)
                     Address = address.strip()
                     Name = From.rsplit(' ', 1)[0]
-                    From = From.replace('\"','')
-
-                    Name = Name.replace('\"','')
-
-
+                    From = From.replace('"', '')
+                    Name = Name.replace('"', '')
 
             if From not in session['seen']:
                 Contact = {
@@ -133,135 +104,157 @@ def contacts(pagenr):
                     'initial': Name[0].upper(),
                     'address': Address,
                     'snippet': results['snippet'],
-                    'unread': Unread
+                    'unread': Unread,
                     }
                 session['contacts'].append(Contact)
                 session['seen'].append(From)
 
-        query = ""
+        #query = ''
 
-        if not session['seen']:
-            pass
-        else:
-            query = "from:("
-            for From in session['seen']:
-                query = query + "-'" + From + "', "
-            query = query + ")"
+        #if not session['seen']:
+        #    pass
+        #else:
+        #    query = 'from:('
+        #    for From in session['seen']:
+        #        query = query + "-'" + From + "', "
+        #    query = query + ')'
 
+        # query = "\"to:'" + contact + "' AND from:me \" OR from:'" + contact + "'"
 
-        #query = "\"to:'" + contact + "' AND from:me \" OR from:'" + contact + "'"
-        message_ids = gmail_service.users().messages().list(userId='me', maxResults=50, pageToken=session['contactsNextPageToken']).execute()
+        message_ids = gmail_service.users().messages().list(userId='me'
+                , maxResults=50,
+                pageToken=session['contactsNextPageToken']).execute()
         batchContacts = BatchHttpRequest(callback=contactscallbackfunc)
-        #return message_ids['messages'][0]['id']
         session['contactsNextPageToken'] = message_ids['nextPageToken']
         for msg_id in message_ids['messages']:
-            batchContacts.add(gmail_service.users().messages().get(userId='me', id=msg_id['id'], format='metadata', metadataHeaders=['from', 'date']))
+            batchContacts.add(gmail_service.users().messages().get(userId='me'
+                              , id=msg_id['id'], format='metadata',
+                              metadataHeaders=['from', 'date']))
         batchContacts.execute()
 
-
-
-    #return "test"
-
-
-    #def mailscallbackfunc(result, results, moreresults):
-    #    if 'UNREAD' in results['labelIds']:
-    #        Unread = 'true'
-    #   else:
-    #        Unread = 'false'
-    #
-    #batchMails = BatchHttpRequest(callback=mailscallbackfunc)
-    #for contact in session['seen']:
-	#	batchContacts.add(gmail_service.users().messages().get(userId='me',
-    #              id=msg_id['id'], format='metadata',
-    #              metadataHeaders=['from', 'date']))
-    #batchMails.execute()
     if pagenr - 1 == 0:
         start = 0
         end = 10
     else:
         start = (pagenr - 1) * 10
         end = pagenr * 10
-    response = {'nextPageToken': session['contactsNextPageToken'], 'contacts': session['contacts'][start:end]}
+    response = {'nextPageToken': session['contactsNextPageToken'],
+                'contacts': (session['contacts'])[start:end]}
     js = json.dumps(response)
     resp = Response(js, status=200, mimetype='application/json')
     return resp
 
+
 @app.route('/v1/gmail/message/<msg_id>')
 def get_message(msg_id):
-    # Authorize the httplib2.Http object with our credentials
+
     credentials = client.OAuth2Credentials.from_json(session['credentials'])
-
-    http = credentials.authorize(httplib2.Http(cache=".cache"))
-
-# Build the Gmail service from discovery
-
+    http = credentials.authorize(httplib2.Http(cache='.cache'))
     gmail_service = discovery.build('gmail', 'v1', http=http)
 
     try:
-        message = gmail_service.users().messages().get(userId='me', id=msg_id,
-                                             format='full').execute()
-
+        message = gmail_service.users().messages().get(userId='me',
+                id=msg_id, format='full').execute()
         message_payload = message['payload']
 
         if 'parts' in message_payload:
-
-            message_parts = message_payload['parts']
-
             for part in message_payload['parts']:
-                    if part['mimeType'] == 'text/html':
-                        message_body = part['body']
-                    if 'parts' in part:
-                        for part in part['parts']:
-                            if part['mimeType'] == 'text/html':
-                                message_body = part['body']
-            msg_str = base64.urlsafe_b64decode(message_body['data'].encode('utf-8'))
+                if (part['mimeType'] == 'text/html') or (part['mimeType'] == 'text/plain'):
+                    print "#0"
+                    message_body = part['body']
+                    for header in part['headers']:
+                        if header['name'] == 'Content-Type':
+                            charset = header['value'].rpartition('charset=')[2]
+                            charset = re.sub('"', '', charset, flags=re.DOTALL)
+                        if header['name'] == 'Content-Transfer-Encoding':
+                            transferencoding = header['value']
+                if 'parts' in part:
+                    print "#1"
+                    for part in part['parts']:
+                        if (part['mimeType'] == 'text/html') or (part['mimeType'] == 'text/plain'):
+                            message_body = part['body']
+                            for header in part['headers']:
+                                if header['name'] == 'Content-Type':
+                                    charset = header['value'].rpartition('charset=')[2]
+                                    charset = re.sub('"', '', charset, flags=re.DOTALL)
+                                if header['name'] == 'Content-Transfer-Encoding':
+                                    transferencoding = header['value']
+
+
+            msg_str = base64.urlsafe_b64decode((message_body['data']).encode('ascii'))
+#            msg_str = msg_str.decode(charset)
+#            msg_str = msg_str.encode('utf-8')
 
         else:
-            message_body=message_payload['body']
-            msg_str = base64.urlsafe_b64decode(message_body['data'].encode('utf-8'))
+
+            message_body = message_payload['body']
+            print "#3"
+            for header in message_payload['headers']:
+                                if header['name'] == 'Content-Type':
+                                    charset = header['value'].rpartition('charset=')[2]
+                                    charset = re.sub('"', '', charset, flags=re.DOTALL)
+                                if header['name'] == 'Content-Transfer-Encoding':
+                                    transferencoding = header['value']
+
+            msg_str = base64.urlsafe_b64decode((message_body['data']).encode('ascii'))
+#            msg_str = msg_str.encode('utf-8')
             msg_str = '<pre>' + msg_str + '</pre>'
 
+        print transferencoding
+        #if transferencoding == 'quoted-printable':
+        #    print "YEEEEEEEEP"
+        #    msg_str = quopri.decodestring(msg_str)
+        #if charset != 'UTF-8':
+        print charset
+        #msg_str = msg_str.decode(charset, errors='ignore')
+        msg_str = unicode(msg_str)
+        msg_str = fix_bad_unicode(msg_str)
+        #msg_str = msg_str.encode('utf-8', errors='ignore')
+        #msg_str = msg_str.decode(charset)
+        #msg_str = msg_str.encode('utf-8')
+
+        #if charset.lower() is not 'utf-8':
 
 
-        msg_str = re.sub("!important", "", msg_str, flags=re.DOTALL)
+        msg_str = re.sub('!important', '', msg_str, flags=re.DOTALL)
+
         transformed_msg = transform(msg_str)
 
-        clean_msg = re.sub("<html>", "", transformed_msg, flags=re.DOTALL)
-        clean_msg = re.sub("</html>", "", clean_msg, flags=re.DOTALL)
-        clean_msg = re.sub('<head>.*?</head>', "", clean_msg, flags=re.DOTALL)
-        clean_msg = re.sub('<head>.*?</head>', "", clean_msg, flags=re.DOTALL)
+        clean_msg = re.sub('<html>', '', transformed_msg,
+                           flags=re.DOTALL)
+        clean_msg = re.sub('</html>', '', clean_msg, flags=re.DOTALL)
+        clean_msg = re.sub('<head>.*?</head>', '', clean_msg,
+                           flags=re.DOTALL)
+        clean_msg = re.sub('<head>.*?</head>', '', clean_msg,
+                           flags=re.DOTALL)
         clean_msg = clean_msg.replace('body', 'div', 1)
-        clean_msg = re.sub('<body>', "", clean_msg, flags=re.DOTALL)
-        clean_msg = re.sub('</body>', "", clean_msg, flags=re.DOTALL)
-        clean_msg = re.sub('<style type="text/css">.*?</style>', "", clean_msg, flags=re.DOTALL)
-        clean_msg = re.sub('<style type="text/css">.*?</style>', "", clean_msg, flags=re.DOTALL)
+        clean_msg = re.sub('<body>', '', clean_msg, flags=re.DOTALL)
+        clean_msg = re.sub('</body>', '', clean_msg, flags=re.DOTALL)
+        clean_msg = re.sub('<style type="text/css">.*?</style>', '',
+                           clean_msg, flags=re.DOTALL)
+        clean_msg = re.sub('<style type="text/css">.*?</style>', '',
+                           clean_msg, flags=re.DOTALL)
 
-        #return msg_str
-        #print 'Message snippet: %s' % message['snippet']
-        #mime_msg = email.message_from_string(msg_str)
-        #if mime_msg.is_multipart():
-        #    for payload in mime_msg.get_payload():
-            # if payload.is_multipart(): ...
-        #        message = payload.get_payload()
-        #return render_template('emailbody.html', emailbody=clean_msg)
         return clean_msg
-        #return message
     except errors.HttpError, error:
+
+        # return message
+
         print 'An error occurred: %s' % error
 
-@app.route("/v1/gmail/messages/<int:contactId>", methods=['GET'])
+
+@app.route('/v1/gmail/messages/<int:contactId>', methods=['GET'])
 def get_messages(contactId):
     contact = session['seen'][contactId]
     credentials = client.OAuth2Credentials.from_json(session['credentials'])
-    http = credentials.authorize(httplib2.Http(cache=".cache"))
+    http = credentials.authorize(httplib2.Http(cache='.cache'))
 
 # Build the Gmail service from discovery
 
     gmail_service = discovery.build('gmail', 'v1', http=http)
+
     def mailscallbackfunc(result, results, moreresults):
-        # in old Python versions:
-        # if seen.has_key(marker)
-        # but in new ones:
+
         if 'UNREAD' in results['labelIds']:
             Unread = True
         else:
@@ -271,12 +264,16 @@ def get_messages(contactId):
                 Date = header['value']
             if header['name'].lower() == 'from':
                 From = header['value']
-                if From == "Anders Damsgaard <andersdm@gmail.com>": #SKAL ÆNDRES
+                if From == 'Anders Damsgaard <andersdm@gmail.com>':  # SKAL ÆNDRES
                     Sent = True
                 else:
                     Sent = False
             if header['name'].lower() == 'subject':
                 Subject = header['value']
+        try:
+            Subject # does a exist in the current namespace
+        except NameError:
+            Subject = 'Unknown Subject' # nope
 
         Contact = {
             'messageId': results['id'],
@@ -284,36 +281,24 @@ def get_messages(contactId):
             'subject': Subject,
             'snippet': results['snippet'],
             'unread': Unread,
-            'sent': Sent
+            'sent': Sent,
             }
 
         session['mails'].append(Contact)
 
-    #for msg_id in message_ids['messages']:
-    #    batchContacts.add(gmail_service.users().messages().get(userId='me',
-    #              id=msg_id['id'], format='metadata',
-    #              metadataHeaders=['from', 'date']))
-    #batchContacts.execute()
-
-    query = "\"to:'" + contact + "' AND from:me \" OR from:'" + contact + "'"
+    query = '"to:\'' + contact + '\' AND from:me " OR from:\'' \
+        + contact + "'"
     message_ids = gmail_service.users().messages().list(userId='me',
             maxResults=10, labelIds='INBOX', q=query).execute()
 
     batchMails = BatchHttpRequest(callback=mailscallbackfunc)
 
     for msg_id in message_ids['messages']:
-        batchMails.add(gmail_service.users().messages().get(userId='me',
-                  id=msg_id['id'], format='metadata',
-                  metadataHeaders=['from', 'date', 'subject']))
+        batchMails.add(gmail_service.users().messages().get(userId='me'
+                       , id=msg_id['id'], format='metadata',
+                       metadataHeaders=['from', 'date', 'subject']))
     batchMails.execute()
     response = {'messages': session['mails']}
     js = json.dumps(response)
     resp = Response(js, status=200, mimetype='application/json')
     return resp
-
-
-
-#if __name__ == '__main__':
-#    app.config['SECRET_KEY'] = 'super secret key'
-#    app.debug = True
-#    app.run()
--- static/view1/view1.js
+++ static/view1/view1.js
@@ -9,79 +9,69 @@ angular.module('myApp.view1', ['ngRoute', 'base64', 'ngResource'])
     });
 }])
 
-
 .controller('View1Ctrl', function ($scope, $http, $resource, $sce, $window, $timeout, $base64, LxNotificationService, LxDialogService) {
-    // Load all registered users
+
+    // Load resources
 
     var contacts = $resource("v1/gmail/contacts/:id");
     var messages = $resource("v1/gmail/messages/:id");
     var message = $resource("v1/gmail/message/:id");
 
+    // Set variables
+
+    var w = angular.element($window);
     $scope.contacts = []
-    $scope.pageNr = 1
+    $scope.rightShow = 'listMails'
+    $scope.leftWrapper = null
+    $scope.rightWrapper = 'hiddenMobile'
+    $scope.currentContact = {
+        name: false,
+        avatar: false
+    }
+    $scope.currentMessage = {
+        subject: '',
+        date: '',
+        sent: ''
+    }
 
-    $scope.getContacts = function(pageNr) {
-    contacts.get({
-        id: pageNr
-    }).$promise.then(function (data) {
-        $scope.contacts = $scope.contacts.concat(data['contacts'])
-        console.log($scope.contacts)
-        $scope.page = {
-            dataLoaded: true
+    $scope.pages = {
+        'contactsPage': 1,
+        'messagesPage':1
+    }
 
-        };
+    // Define functions
+
+    $scope.getContacts = function (pageNr) {
+        contacts.get({
+            id: pageNr
+        }).$promise.then(function (data) {
+            $scope.contacts = $scope.contacts.concat(data['contacts'])
+            console.log($scope.contacts)
+            $scope.page = {
+                dataLoaded: true
+
+            };
             $scope.stretchLeft();
-        $scope.pageNr = $scope.pageNr + 1
-
-        var i
-        $scope.messageslist = []
-        angular.forEach($scope.contacts, function (value, key) {
-            messages.get({
-                id: key
-            }).$promise.then(function (data) {
-                $scope.messageslist[key] = data['messages'];
-            }, console.log('test'));
-
-        });
-    })
-}
-
-$scope.getContacts(1)
+            $scope.pages['contactsPage'] = $scope.pages['contactsPage'] + 1
+            var i
+            $scope.messageslist = []
+            angular.forEach($scope.contacts, function (value, key) {
+                messages.get({
+                    id: key
+                }).$promise.then(function (data) {
+                    $scope.messageslist[key] = data['messages'];
+                }, console.log('test'));
+            });
+        })
+    }
 
     $scope.getMessage = function (msgId) {
         $http.get('/v1/gmail/message/' + msgId).
         success(function (data) {
             $scope.emailbody = $sce.trustAsHtml(data);
+        })
+    };
 
-
-        })};
-
-
-
-
-
-    $scope.mobileBack = function () {
-        $scope.leftWrapper = null;
-        $scope.rightWrapper = 'hiddenMobile';
-    }
-
-    $scope.leftWrapper = null
-    $scope.rightWrapper = 'hiddenMobile'
-
-    /*$http.get('/v1/gmail/contacts/1').
-        success(function(data) {
-            $scope.contacts = data['contacts'];
-        $timeout(function() {
-        $scope.page = {dataLoaded: true};
-        },1000);
-        });
-    $http.get('/v1/gmail/listmessages').
-                success(function(data) {
-                $scope.messageslist = data;
-
-
-        });
-    */
     $scope.getMessages = function (id, pagenr, name, address, initial) {
         $scope.messages = $scope.messageslist[id];
         $scope.rightShow = 'listMails';
@@ -91,36 +81,16 @@ $scope.getContacts(1)
             initial: initial
         };
         $timeout(function () {
-        $scope.leftWrapper = 'hiddenMobile';
-        $scope.rightWrapper = null;
-
-
-        }, 150);
-        /*
-		$http.get('/v1/gmail/messages/' + id + '/' + pagenr).
-        success(function(data) {
-            $scope.messages = data;
-            $scope.rightShow = 'listMails';
             $scope.leftWrapper = 'hiddenMobile';
             $scope.rightWrapper = null;
-            $scope.currentContact = {name: name, address: address };
-        }); */
-
-            $scope.stretchRight();
-
+        }, 150);
+        $scope.stretchRight();
     }
 
-    $scope.currentContact = {
-        name: false,
-        avatar: false
+    $scope.mobileBack = function () {
+        $scope.leftWrapper = null;
+        $scope.rightWrapper = 'hiddenMobile';
     }
-
-    $scope.currentMessage = {
-        subject: '',
-        date: '',
-        sent: ''
-    }
-    $scope.rightShow = 'listMails'
 
     $scope.changeRight = function (what) {
         $timeout(function () {
@@ -128,92 +98,24 @@ $scope.getContacts(1)
         }, 150);
     }
 
-
-
-    $scope.write = {
-        contact: ''
-    }
-
     $scope.urldecode = function (url) {
         return decodeURIComponent(url)
     }
 
-
-    $scope.switchTheme = function () {
-        var color = $scope.theme['themeColor'];
-
-        if ($scope.theme['themeColor'] === 'red') {
-            color='blue'
-        }
-        if ($scope.theme['themeColor'] === 'blue') {
-            color='red'
-        }
-
-        $scope.theme = {headerColor: 'header-bg-' + color,
-                        iconColor: 'icon-color-' + color,
-                        themeColor: color
-        }
-    }
-
-    $scope.setTheme = function (color) {
-
-
-        $scope.theme = {headerColor: 'header-bg-' + color,
-                        iconColor: 'icon-color-' + color,
-                        themeColor: color
-        }
-    }
-
-
-    $scope.setTheme('blue');
-
-    $scope.random = function (array) {
-        var m = array.length,
-            t, i;
-
-        // While there remain elements to shuffle
-        while (m) {
-            // Pick a remaining element…
-            i = Math.floor(Math.random() * m--);
-
-            // And swap it with the current element.
-            t = array[m];
-            array[m] = array[i];
-            array[i] = t;
-        }
-
-        return array;
-    }
-    var w = angular.element($window);
-
     $scope.stretchLeft = function () {
-
+        var w = angular.element($window);
         $scope.scrollbarHeightLeft = w.innerHeight() - $("#contacts").offset().top + 'px'
-
     }
 
     $scope.stretchRight = function () {
-
+        var w = angular.element($window);
         $scope.scrollbarHeightRight = w.innerHeight() - $("#contacts").offset().top + 'px'
-
     }
 
+    // Run functions
+    $scope.getContacts($scope.pages['contactsPage'])
 
-
-
-
-
-    $scope.opendDialog = function (dialogId) {
-        LxDialogService.open(dialogId);
-    };
-
-    $scope.closingDialog = function () {
-        LxNotificationService.info('Dialog closed!');
-    };
-    //http://stackoverflow.com/questions/15458609/execute-function-on-page-load
-
-    var w = angular.element($window);
-
+    //Bind functions
     w.bind('resize', function () {
         $scope.$apply(function () {
             $scope.stretchLeft();
